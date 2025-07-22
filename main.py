78947841482ec0598bfc0db8470895275230d923
Dumthr <aryark46@gmail.com>
2025-07-22 08:52:48 +0000

    modified main.py add get_book_text func

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(path):
+    with open(path) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    get_book_text("books/frankenstein.txt")
+
+if __name__ == "__main__":
+    main()
